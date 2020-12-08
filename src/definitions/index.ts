d972f7868c2a4f84e4ae4400cdd8067f4f5d366e
czq <chenzhengqiang@youzan.com>
2020-12-08 20:39:57 +0800

    fix: 修改定义

--- src/definitions/index.ts
+++ src/definitions/index.ts
@@ -2,7 +2,7 @@ import { HttpStatus } from '../constants';
 import { Context } from 'koa';
 
 export interface PrickingCtx extends Context {
-  json(code: HttpStatus, msg: string, data: Record<string, any>): Promise<void>;
-  success(data: Record<string, any>): Promise<void>;
+  json(code: HttpStatus, msg: string, data: any): Promise<void>;
+  success(data: any): Promise<void>;
   fail(msg?: string): Promise<void>;
 }
