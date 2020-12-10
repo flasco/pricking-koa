9684fab41ef2322a88aaa1c338e5cbd710e7bc2f
czq <chenzhengqiang@youzan.com>
2020-12-10 17:05:46 +0800

    feat: support validator

--- src/controllers/BaseController.ts
+++ src/controllers/BaseController.ts
@@ -1,7 +1,10 @@
 import { PrickingCtx } from '../definitions';
+import Validator from '../lib/validator';
 
 class BaseController<T extends PrickingCtx = PrickingCtx> {
   ctx: T;
+  validator = new Validator();
+
   constructor(ctx: T) {
     this.ctx = ctx;
   }
---
+++ src/lib/validator.ts
@@ -0,0 +1,23 @@
+class Validators {
+  required(value: any, msg = '参数不得为空') {
+    if (typeof value === 'number') return this;
+    if (typeof value === 'string') {
+      value = value.trim();
+    }
+
+    if (!value || value === 'null' || value === 'undefined') {
+      throw new Error(msg);
+    }
+    return this;
+  }
+
+  isNumber(value: any, msg = '参数必须为数字') {
+    const numeric = /^[+-]?([0-9]*[.])?[0-9]+$/;
+    if (!numeric.test(value)) {
+      throw new Error(msg);
+    }
+    return this;
+  }
+}
+
+export = Validators;
