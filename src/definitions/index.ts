efefa4d4e60f574a01ddb19f6f1e90ab795e3526
czq <chenzhengqiang@youzan.com>
2021-01-21 16:23:37 +0800

    fix: 补充定义

--- src/definitions/index.ts
+++ src/definitions/index.ts
@@ -1,8 +1,12 @@
 import { HttpStatus } from '../constants';
-import { Context } from 'koa';
+import { Context, Request } from 'koa';
 
+interface IReq extends Request {
+  body: any;
+}
 export interface PrickingCtx extends Context {
   json(code: HttpStatus, msg: string, data: any): Promise<void>;
   success(data: any): Promise<void>;
   fail(msg?: string): Promise<void>;
+  request: IReq;
 }
