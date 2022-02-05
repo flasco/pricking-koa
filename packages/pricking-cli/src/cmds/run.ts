50f5f1abd3fd6c7f8dbd36a6a034b5d4338a7584
flasco <flasco@163.com>
2022-02-05 14:29:31 +0800

    fix: 修改

--- packages/pricking-cli/src/cmds/run.ts
+++ packages/pricking-cli/src/cmds/run.ts
@@ -34,7 +34,9 @@ const objInject = (key: string, value: any, defaultValue?: any) => {
 
 const convertConf = (confPath: string, pconf: IPrickingConf, argv: any) => {
   const entryPath = path.join(path.dirname(confPath), pconf.entryPoint);
-  const execInstArr = ['esno', '-r tsconfig-paths/register'];
+  const register = require.resolve('esbuild-register');
+
+  const execInstArr = ['node', `-r ${register}`, '-r tsconfig-paths/register'];
 
   if (pconf.execArgs && Array.isArray(pconf.execArgs)) {
     execInstArr.push(...pconf.execArgs.map(i => (typeof i === 'string' ? i : i.join(' '))));
